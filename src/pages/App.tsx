3c2b2a18ac183e06f20890a7296a2a026edec6a6
hacryptoha <minatotanaka512@gmail.com>
2022-07-06 18:01:36 -0600

    update_6

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-// @ts-nocheck
+ // @ts-nocheck
 import Loader from 'components/Loader'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
 import { Suspense, useRef, useEffect, useCallback, useState } from 'react'
